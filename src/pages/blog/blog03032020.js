fb209f83a4a718b333c6186f82fa6734444ff632
kawasakileo <leonardokws@gmail.com>
2020-03-05 20:28:15 -0300

    Blog entry 03/03

---
+++ src/pages/blog/blog03032020.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const blog03032020 = () => (
+    <Layout>
+        <SEO title="blog03032020" />
+        <h1>blog03032020</h1>
+        <p>
+            Utilizando o conhecimento que ja obtinha sobre HTML e CSS, testei alguns estilos para as blog entries.
+        </p>
+        <p>
+            Apliquei um Hover ao link, em que sua caixa mudava sua cor de branco para azul. Também utilizei um shadow
+            para destacar ainda mais o botão quando o mouse estiver apontado nele.
+        </p>
+        <p>
+            Por fim adicionei um timer nesses links e também na barra de menu (que fica em baixo do header).
+        </p>
+    </Layout>
+)
+
+export default blog03032020
