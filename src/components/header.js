e3e594255145a78b87aaf0face12bc9dd8c9c21e
kawasakileo <leonardokws@gmail.com>
2020-03-28 14:21:56 -0300

    Header responsivo

--- src/components/header.js
+++ src/components/header.js
@@ -4,33 +4,42 @@ import React from "react"
 
 // TODO: deixar o header responsivo
 const Header = ({ siteTitle }) => (
-  <header style={{ background: `linear-gradient(#0074D9, #33a0ff)`, marginBottom: `2.2%`,
-                paddingTop: `1%`, paddingBottom: `0%`, paddingLeft: `0%`, paddingRight: `0%` }}>
+  <header style={{
+    background: `linear-gradient(#0074D9, #33a0ff)`, marginBottom: `2.2%`,
+    paddingTop: `1.3%`, paddingBottom: `0%`, paddingLeft: `0%`, paddingRight: `0%`
+  }}>
     <div style={{ marginLeft: `16%`, display: `inline-block` }}>
       <h1>
-        <Link to="/" style={{ fontSize: `32px`, color: `white`, textDecoration: `none` }}>
+        <Link to="/" style={{ fontSize: `46px`, color: `white`, textDecoration: `none` }}>
           <p class="headerP">{siteTitle}</p>
         </Link>
       </h1>
     </div>
-    <div style={{ marginLeft: `37%`, display: `inline-block` }}>
+    <div style={{ display: `inline-block`, float: `right`, marginRight: `16%` }}>
       <h3>
-        <Link to="/projetos/" style={{ fontWeight: 450, color: `white`, textDecoration: `none` }}>
-          <p class="headerP">Projetos</p>
+        <Link to="/contato/" style={{ color: `white`, fontWeight: 450, textDecoration: `none` }}>
+          <p class="headerP">Contato</p>
         </Link>
       </h3>
     </div>
-    <div style={{ marginLeft: `1%`, display: `inline-block` }}>
+    <div style={{ display: `inline-block`, float: `right`, marginRight: `1%`}}>
       <h3>
         <Link to="/sobre/" style={{ fontWeight: 450, color: `white`, textDecoration: `none` }}>
           <p class="headerP">Sobre</p>
         </Link>
       </h3>
     </div>
-    <div style={{ marginLeft: `1%`, display: `inline-block` }}>
+    <div style={{ display: `inline-block`, float: `right`, marginRight: `1%` }}>
       <h3>
-        <Link to="/contato/" style={{ color: `white`, fontWeight: 450, textDecoration: `none` }}>
-          <p class="headerP">Contato</p>
+        <Link to="/projetos/" style={{ fontWeight: 450, color: `white`, textDecoration: `none` }}>
+          <p class="headerP">Tech Stack</p>
+        </Link>
+      </h3>
+    </div>
+    <div style={{ display: `inline-block`, float: `right`, marginRight: `1%` }}>
+      <h3>
+        <Link to="/projetos/" style={{ fontWeight: 450, color: `white`, textDecoration: `none` }}>
+          <p class="headerP">Projetos</p>
         </Link>
       </h3>
     </div>
