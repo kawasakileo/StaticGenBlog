2a1de8b8ed20ff2a0126680a815a2a124346d07e
kawasakileo <leonardokws@gmail.com>
2020-06-01 22:29:25 -0300

    Correção no e-mail

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -8,7 +8,7 @@ const Contato = () => (
       <h1 class="centerH1">Contato</h1>
       <div class="postagem" style={{ marginLeft: `200px`}}>
       <h4><p>Email: leonardokws@edu.unifil.br</p></h4>
-      <h4><p>Email: webmaster@blogpi2.online</p></h4>
+      <h4><p>Email: fwdwebmaster@blogpi2.online</p></h4>
       <h4 style={{ marginBottom: `35px` }}><p>Redes Sociais: <a 
           target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/leonardo-kawasaki/"> LinkedIn</a> | <a 
           target="_blank" rel="noopener noreferrer" href="https://github.com/kawasakileo">GitHub</a></p></h4>
